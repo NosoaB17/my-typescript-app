7270102016c49cb4a40cb0192f75cd0556b684d8
NosoaB17 <ngsn176@gmail.com>
2024-11-21 16:13:40 +0700

    ShowMapAp p

---
+++ learning/types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prefer-const */
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+let red = new Color();
